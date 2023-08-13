95e322a242892961eb4b6bca39e5bac915f2c272
mserafin20 <mmserafin20@gmail.com>
2023-08-13 14:29:03 -0400

    added Reaction model

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,4 +1,45 @@
-// const { Schema, Types } = require('mongoose');
+const { Schema, Types } = require('mongoose');
 
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            minlength: 10,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: formatDate,
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
 
-// module.exports = reactionSchema;
+// Getter to format date
+
+function formatDate(createdAt) {
+    return createdAt.toLocaleDateString("en-US", {
+        day: "2-digit",
+        year: "numeric",
+        month: "long",
+        hour: "2-digit",
+        minute: "2-digit",
+    });
+};
+
+module.exports = reactionSchema;
